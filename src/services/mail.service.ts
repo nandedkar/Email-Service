95799184e23576cde93a37c7346a8e411fa060e6
mucchekar <manoj_ucchekar@magicsoftware.com>
2026-07-03 23:48:24 +0530

    4th commit : Add logo and invoice attachments to welcome email

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 import { emailConfig } from "../config/email";
 import { compileTemplate, loadTemplate } from "../utils/templates";
 import { WelcomeEmailData } from "../types/mail.types";
+import path from "path/win32";
 
 export enum EmailTemplate {
   Welcome = "welcome",
@@ -30,12 +31,31 @@ class MailService {
     const { to, name, loginUrl } = data;
     const html = await loadTemplate(EmailTemplate.Welcome);
     const compiledHtml = compileTemplate(html, { name, loginUrl });
+    const invoicePath = path.join(
+      process.cwd(),
+      "files",
+      "sample.pdf",
+    );
+    const logoPath = path.join(process.cwd(), "files", "logo.png");
     return this.sendMail({
       from: `Workspace Team <${emailConfig.user}>`,
       to: to,
       subject: "Welcome to Workspace!",
       text: `Hello ${name},\n\nWelcome to Workspace!`,
       html: compiledHtml,
+      attachments: [
+        {
+          filename: "sample.pdf",
+          path: invoicePath,
+          contentType: "application/pdf",
+        },
+        {
+          filename: "logo.png",
+          path: logoPath,
+          contentType: "image/png",
+          cid: "company-logo", // same cid value as in the html img src},
+        },
+      ],
     });
   }
 }
